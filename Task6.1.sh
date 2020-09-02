d11f24d79b8dd1d741d1bc8e3d0251bfc7d2739d
DenisPt <67507367+DenisPt@users.noreply.github.com>
2020-09-03 00:21:20 +1000

    Create Task6.1.sh

---
+++ Task6.1.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+manual(){
+        cat << EOF
+This script erase empty strings and converts text to uppercase
+EOF
+}
+
+W=0 #for writing in files or not
+while [[ $# > 0 ]]
+do
+        case $1 in
+                --help)
+                        manual
+                        exit 0
+                ;;
+                -w|-write)
+                        W=1
+                ;;
+                *)
+                        if [[ -f $1|| -h $1 ]]
+                        then
+                                FILES+=($1)
+                        else
+                                WRONGS+=($1)
+                        fi
+                ;;
+        esac
+        shift
+done
+
+if [[ ${#WRONGS[@]} == 0 ]]
+then
+        for file in $FILES
+        do
+                if [ $W == 0 ]
+                then
+                        sed '/^$/d' $file | tr 'a-z' 'A-Z'
+                else
+                        sed '/^$/d' $file | tr 'a-z' 'A-Z' > file_tmp
+                        cat file_tmp > $file
+                        rm file_tmp
+                fi
+        done
+else
+        echo 'There is error parametrs: '${WRONGS[@]}
+        exit 1
+                                                                              1,9           Top
